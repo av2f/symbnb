c973770bbcee7e523cc7eb7a9079ab90b938c275
av2f <fparmentier@free.fr>
2019-06-28 19:10:56 +0200

    2eme commit avant envoie

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ad;
+use Faker\Factory;
+use Cocur\Slugify\Slugify;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $faker=Factory::create('fr_FR');
+        $slugify=new Slugify();
+       
+        for ($i=1; $i<=30; $i++){
+            $ad=new Ad();
+
+            $title = $faker->sentence();
+            $slug=$slugify->slugify($title);
+
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
+
+            $ad->setTitle($title)
+                ->setSlug($slug)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(mt_rand(40,200))
+                ->setRooms(mt_rand(1,6));
+
+            $manager->persist($ad);
+        }   
+        $manager->flush();
+    }
+}
