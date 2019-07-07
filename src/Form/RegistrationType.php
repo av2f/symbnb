b6637bf7bc29857b8377159ecba18b767bdd52b4
unknown <fparmentier@free.fr>
2019-07-07 21:19:07 +0200

    sunday training

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class RegistrationType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'firstName',
+                TextType::class,
+                $this->getConfiguration("Prénom", "votre prénom...")
+            )
+            ->add(
+                'lastName',
+                TextType::class,
+                $this->getConfiguration("Nom", "votre nom de famille...")
+            )
+            ->add(
+                'email',
+                EmailType::class,
+                $this->getConfiguration("eMail","votre email...")
+            )
+            ->add(
+                'picture',
+                UrlType::class,
+                $this->getConfiguration("Photo de profil", "Url de votre avatar")
+            )
+            ->add(
+                'hash',
+                PasswordType::class,
+                $this->getConfiguration("Mot de passe", "Votre mot de passe")
+            )
+            ->add(
+                'passwordConfirm',
+                PasswordType::class,
+                $this->getConfiguration("Confirmation du mot de passe", "Veuillez confirmez votre mot de passe")
+            )
+            ->add(
+                'introduction',
+                TextType::class,
+                $this->getConfiguration("Introduction", "Présentez-vous en quelques mots")
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                $this->getConfiguration("Description détaillée", "C'est le moment de vous présenter")
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
