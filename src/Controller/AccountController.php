de6f13b9cc48d15ddb5e884d4bad400608daadd0
unknown <fparmentier@free.fr>
2019-07-08 09:00:04 +0200

    gestion mot de passe

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
 use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
@@ -75,6 +78,54 @@ class AccountController extends AbstractController
         }
 
         return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher et de traiter le formulaire de modification de profil
+     * 
+     * @Route("/account/profile", name="account_profile")
+     *
+     * @return Response
+     */
+    public function profile(Request $request, ObjectManager $manager) {
+
+        $user=$this->getUser();
+        $form=$this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les données du profil ont été modifiées avec succès"
+            );
+        }
+               
+        return $this->render('account/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * Permet de modifier le mot de passe
+     * 
+     * @Route("/account/password-update", name="account_password")
+     *
+     * @return Response
+     */
+    public function  updatePassword()
+    {
+        $passwordUpdate=new PasswordUpdate();
+
+        $form=$this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        return $this->render('account/password.html.twig', [
             'form' => $form->createView()
         ]);
     }
