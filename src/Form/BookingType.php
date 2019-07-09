6deaefd2892ed72394113ef38bacd7174aed3d7d
av2f <fparmentier@free.fr>
2019-07-09 17:42:28 +0200

    début de la gestion des réservations

---
+++ src/Form/BookingType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class BookingType extends ApplicationType
+// rappel : ApplicationType contient la fonction getConfiguration et herite de AbstractType
+
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'startDate',
+                DateType::class,
+                $this->getConfiguration("Date d'arrivée", "Date à laquelle vous arrivez",
+                ["widget"=>"single_text"])
+            )
+            ->add(
+                'endDate',
+                DateType::class,
+                $this->getConfiguration("Date de départ", "Date à laquelle vous partez",
+                ["widget"=>"single_text"])
+            )
+            // false pour label = pas de label
+            ->add(
+                'comment',
+                TextareaType::class,
+                $this->getConfiguration(false,"Si vous avez un commentaire, n'hésitez pas à en faire part")
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
