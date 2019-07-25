bb18a6f52d108d7cb4c0148721a5c4e87074c563
unknown <fparmentier@free.fr>
2019-07-25 08:34:39 +0200

    201907250834

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,14 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(AdRepository $adRepo, UserRepository $userRepo)
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'ads'=>$adRepo->findBestAds(3),
+            'users'=>$userRepo->findBestUsers(2)
+        ]);
     }
 }
